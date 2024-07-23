298f6f1ba3a355185ebdf7782f4dbb6ffe4c9ed3
rolandash <ning.roland@gmail.com>
2024-07-23 16:53:42 +0800

    Move ktd202x driver into airmind board specific directory.

--- boards/airmind/mindracer_gen3_finch/src/board_config.h
+++ boards/airmind/mindracer_gen3_finch/src/board_config.h
@@ -123,6 +123,8 @@
 #define ADC_HW_VER_SENSE_CHANNEL            /* PA2 */  ADC1_CH(2)
 #define ADC_HW_REV_SENSE_CHANNEL            /* PC1 */  ADC1_CH(11)
 
+#define SYSTEM_ADC_BASE STM32_ADC1_BASE
+
 #if BOARD_HAS_NBAT_V == 2 && BOARD_HAS_NBAT_I == 2
 #define ADC_CHANNELS \
 	((1 << ADC_BATTERY1_VOLTAGE_CHANNEL)       | \
---
+++ boards/airmind/mindracer_gen3_finch/src/drivers/adj_bec/adj_bec_params.c
@@ -0,0 +1,10 @@
+
+/**
+ * Enable BEC output voltage adjustment.
+ *
+ * @category System
+ * @reboot_required true
+ * @boolean
+ * @group BEC
+ */
+PARAM_DEFINE_INT32(ADJ_BEC_EN, 1);
